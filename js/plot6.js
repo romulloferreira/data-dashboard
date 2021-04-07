44b4eadc947e4c78a8bd744ca7a1714bd2b20a34
Romullo Ferreira <romulloferreira@gmail.com>
2021-04-06 22:06:32 -0300

    add plot6.js file, cumulative deaths total plot

---
+++ js/plot6.js
@@ -0,0 +1,30 @@
+var dates = ['13/03', '14/03', '15/03', '16/03', '17/03', '18/03', '19/03'];
+
+var deaths_brazil = [277102, 278229, 279286, 282127, 284775, 287499, 290314];
+
+var country_name_brazil = 'Dates';
+
+var trace1 = {
+  x:dates,
+  y:deaths_brazil,
+  mode: 'lines+markers',
+  type: 'scatter',
+  
+  
+};
+
+
+var layout = {
+  title:'Total cumulative coronavirus deaths on 2021-03-19',
+  xaxis: {
+    title: 'date',
+    
+  },
+  yaxis: {
+    title: 'Total coronavirus deaths',
+  }
+};
+
+var data = [trace1];
+
+Plotly.newPlot('plot6', data, layout);
